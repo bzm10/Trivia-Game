c80e1d331c8965a86a3628e3f74c4a5e9b9a7b4a
Benjamin M <137508630+bzm10@users.noreply.github.com>
2024-01-16 20:14:14 +0200

    Update main.py
    Added Comments and background color

--- main.py
+++ main.py
@@ -2,42 +2,49 @@ import requests
 import tkinter as tk
 from tkinter import ttk
 import tkinter.messagebox as messagebox
-import time
 import random
 import html
 import time
 import pygame
 import webbrowser
+import os
 
-# Fetching categories from API
+# Constants for file paths
+AUDIO_PATH = r"/Volumes/Benjamin/Coding/Python/GUI/trivia-game/audio/"
+CORRECT_SOUND_PATH = os.path.join(AUDIO_PATH, "correct.wav")
+INCORRECT_SOUND_PATH = os.path.join(AUDIO_PATH, "incorrect.wav")
+GAME_OVER_SOUND_PATH = os.path.join(AUDIO_PATH, "game_over.wav")
+
+# this function fetches the data from the API and starts the game
 def fetch_data():
-    global difficulty
-    global categorys
-    global menu
-    global question_amount
+    global difficulty_var
+    global category_var
+    global question_amount_var
+    global game_type_var
     global response
 
-    type_of_game = game_type_list.get(game_type.get())
+    # Fetching game type from menu for the API
+    type_of_game = game_type_list.get(game_type_var.get())
 
     # Fetching category from menu for the API
-    category = categorys.get(menu.get())
+    category = categorys.get(category_var.get())
     if category is None:
         messagebox.showerror("Error", "Please select a category.")
         # You can choose to return or exit the function here to prevent further execution
         return
 
     # Fetching difficulty from menu for the API
-    if difficulty.get() == "Easy":
-        difficulty_url = "&difficulty=easy"
-    elif difficulty.get() == "Medium":
-        difficulty_url = "&difficulty=medium"
-    elif difficulty.get() == "Hard":
-        difficulty_url = "&difficulty=hard"
+    if difficulty_var.get() == "Easy":
+        difficulties = "&difficulty=easy"
+    elif difficulty_var.get() == "Medium":
+        difficulties = "&difficulty=medium"
+    elif difficulty_var.get() == "Hard":
+        difficulties = "&difficulty=hard"
     else:
-        difficulty_url = ""
+        difficulties = ""
 
     # Fetching question amount from menu for the API
-    question_amount = question_amounts.get()
+    question_amount = question_amount_var.get()
     if question_amount == "5":
         question_amount = 5
     elif question_amount == "10":
@@ -47,17 +54,21 @@ def fetch_data():
     elif question_amount == "25":
         question_amount = 25
     else:
+        # Display an error message if the question amount is not selected
         messagebox.showerror("Error", "Please select an amount from the list.")
         # You can choose to return or exit the function here to prevent further execution
         return
 
     # Fetching quiz questions from API
-    request = requests.get(f"https://opentdb.com/api.php?amount={question_amount}&category={category}{difficulty_url}&type={type_of_game}")
+    request = requests.get(f"https://opentdb.com/api.php?amount={question_amount}&category={category}{difficulties}&type={type_of_game}")
     response = request.json()
 
+    # Get the response code from the API request
     response_code = response.get('response_code', 5)
 
+    # Check if the API request was successful
     if response_code != 0:
+        # Display an error message if the API request was unsuccessful
         messagebox.showerror("Error", "An error occured while fetching the questions. Please restart the program.")
         # You can choose to return or exit the function here to prevent further execution
         return
@@ -65,13 +76,14 @@ def fetch_data():
     # Start the game
     start_game()
 
+# this function starts the game and run all the game logic
 def start_game():
     # clearing the window
     title.pack_forget()
-    menu.pack_forget()
-    difficulty.pack_forget()
-    question_amounts.pack_forget()
-    game_type.pack_forget()
+    category_menu.pack_forget()
+    difficulty_optionmenu.pack_forget()
+    question_amount_optionmenu.pack_forget()
+    game_type_optionmenu.pack_forget()
     menu_continue.pack_forget()
     timer_checkbox.pack_forget()
 
@@ -84,40 +96,51 @@ def start_game():
     score = 0
     timer = 15 # Timer in seconds
 
-    # Function calulated the score
+    
+    # this function is used to add points to the score
     def score_points():
         global score
-        if question_amount == 5:
+        global question_amount_var
+        # Get the selected question amount
+        selected_question_amount = int(question_amount_var.get())
+
+        # Score calculation
+        if selected_question_amount == 5:
             score += 20
-        elif question_amount == 10:
+        elif selected_question_amount == 10:
             score += 10
-        elif question_amount == 20:
+        elif selected_question_amount == 20:
             score += 5
-        elif question_amount == 25:
+        elif selected_question_amount == 25:
             score += 4
 
-    # Function to check if the selected answer is correct
+    
+    # this function checks if the selected answer is correct or not and displays the correct message accordingly
     def check_answer(selected_answer):
+        global game_index
+        global score
+        
         # audio for the correct and incorrect answer
         def play_sound(file_path):
             pygame.mixer.init()
             pygame.mixer.music.load(file_path)
             pygame.mixer.music.play()
 
-        
-        global game_index
-        global score
-
+        # Get the correct answer from the API response
         correct_answer = html.unescape(response.get("results")[game_index].get("correct_answer"))
 
         # Check if the selected answer is correct
         if selected_answer == correct_answer:
-            # if your on windows you will need to add a r"path-to-audio"
-            play_sound("path-to-audio-file-correct")
+            # Play the correct sound
+            play_sound(CORRECT_SOUND_PATH)
+            # Display the correct message if the selected answer is correct
             messagebox.showinfo("Correct", "Congratulations! You got the question right.")
+            # Add points to the score
             score_points()
         else:
-            play_sound("path-to-audio-file-incorrect")
+            # Play the incorrect sound
+            play_sound(INCORRECT_SOUND_PATH)
+            # Display the correct answer if the selected answer is incorrect
             messagebox.showinfo("Incorrect", "Incorrect answer. The correct answer is: " + correct_answer)
 
 
@@ -127,19 +150,22 @@ def start_game():
         if game_index < len(response.get("results")):
             show_next_question()
         else:
-            # if your on windows you will need to add a r"path-to-audio"
-            play_sound("path-to-audio-file-game-over")
+            # Cancel the timer that is still running in the background
+            global game_over
+            game_over = True
+            
+            # Display the game over message and play the game over sound
+            play_sound(GAME_OVER_SOUND_PATH)
             messagebox.showinfo("Game Over", "You have answered all the questions. Your score is " + str(score) + ".")
+
             # Clear the window
             for widget in window.winfo_children():
                 widget.pack_forget()
 
-            # Cancel the timer that is still running in the background
-            global game_over
-            game_over = True
-
+            # Go back to the main menu
             main()
 
+    # this function displays all the questions and answers on the screen and also displays the timer, progress bar and score
     def show_next_question():
         # initializing the timer starting time
         global start_time
@@ -159,7 +185,7 @@ def start_game():
 
             # Display the timer countdown
             if timer_var.get() == 1:
-                timer_label = tk.Label(window, text=f"Timer: {timer}", font=("Arial", 15))
+                timer_label = tk.Label(window, text=f"Timer: {timer}", font=("Arial", 15),bg=bg)
                 timer_label.pack(pady=5)
                 countdown(timer, timer_label)
 
@@ -170,20 +196,22 @@ def start_game():
             progress = (game_index + 1) / len(response.get("results")) * 100
             progress_bar['value'] = progress
 
-            score_label = tk.Label(window, text="Score: " + str(score) + "%", font=("Arial", 12))
+            # Display the score
+            score_label = tk.Label(window, text="Score: " + str(score) + "%", font=("Arial", 12),bg=bg)
             score_label.pack(pady=5)
 
-
+            # Get the answers and shuffle them
             answers = response.get("results")[game_index].get("incorrect_answers")
             answers.append(correct_answer)
             random.shuffle(answers)
 
             # Display the question and answers
-            question_label = tk.Label(window, text=html.unescape(question), font=("Arial", 20), wraplength=400)
+            question_label = tk.Label(window, text=html.unescape(question), font=("Arial", 20), wraplength=400,bg=bg)
             question_label.pack(pady=20)
 
+            # Display the buttons
             for answer in answers:
-                button = tk.Button(window, text=html.unescape(answer), font=("Arial", 13), command=lambda ans=answer: check_answer(ans))
+                button = tk.Button(window, text=html.unescape(answer), font=("Arial", 13), command=lambda ans=answer: check_answer(ans),highlightbackground=bg)
                 button.pack(pady=2)
 
         # True/False game type
@@ -191,39 +219,40 @@ def start_game():
 
             # Display the timer countdown
             if timer_var.get() == 1:
-                timer_label = tk.Label(window, text=f"Timer: {timer}", font=("Arial", 15))
+                timer_label = tk.Label(window, text=f"Timer: {timer}", font=("Arial", 15),bg=bg)
                 timer_label.pack(pady=5)
                 countdown(timer, timer_label)
 
+            # Display the progress bar
             progress_bar = ttk.Progressbar(window, length=300, mode='determinate')
             progress_bar.pack(pady=5)
 
             progress = (game_index + 1) / len(response.get("results")) * 100
             progress_bar['value'] = progress
 
-            score_label = tk.Label(window, text="Score: " + str(score) + "%", font=("Arial", 12))
+            # Display the score
+            score_label = tk.Label(window, text="Score: " + str(score) + "%", font=("Arial", 12),bg=bg)
             score_label.pack(pady=5)
 
             # Display the question
-            question_label = tk.Label(window, text=html.unescape(question), font=("Arial", 20), wraplength=400)
+            question_label = tk.Label(window, text=html.unescape(question), font=("Arial", 20), wraplength=400,bg=bg)
             question_label.pack(pady=20)
 
             # Display the true/false buttons
-            true_button = tk.Button(window, text="True", font=("Arial", 13), command=lambda: check_answer("True"))
+            true_button = tk.Button(window, text="True", font=("Arial", 13), command=lambda: check_answer("True"),highlightbackground=bg)
             true_button.pack(pady=2)
 
-            false_button = tk.Button(window, text="False", font=("Arial", 13), command=lambda: check_answer("False"))
+            false_button = tk.Button(window, text="False", font=("Arial", 13), command=lambda: check_answer("False"),highlightbackground=bg)
             false_button.pack(pady=2)
     
     # These variables are used to tell the timer that the game ins't over yet 
     global game_over
     game_over = False    
 
-    # Timer function that runs in the background and updates the timer label    
+    # this function is used to display the timer countdown 
     def countdown(seconds, label):
-        global update_timer
-
         def update_timer():
+            # Calculate the remaining time
             elapsed_time = time.time() - start_time
             remaining_seconds = max(0, int(seconds - elapsed_time))
             label.config(text=f"Timer: {remaining_seconds}")
@@ -234,6 +263,7 @@ def start_game():
                 # Stop the timer that is running in the background when the game is over
                 return
             else:
+                # Display a message box when the timer runs out
                 label.config(text="Time's up!")
                 messagebox.showinfo("Time's Up", "Sorry, time's up!")
                 check_answer("")
@@ -246,38 +276,49 @@ def start_game():
 window = tk.Tk()
 window.title("Quiz")
 
+# Window size
 window_width = 500
 window_height = 500
 
+# Window position
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
+# Center the window
 x = (screen_width - window_width) // 2
 y = (screen_height - window_height) // 2
 
+# Set the window size and position
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-# Main menu
+# Disable window resizing
+window.resizable(False, False)
+
+# Window background color
+bg = "dark blue"
+window.configure(bg=bg)
+
+# this function is used to display all the content in the main window
 def main():
     # Global variables
-    global title
-    global category_label
-    global menu
-    global difficulty_label
-    global difficulty
-    global question_amount_label
-    global question_amounts
-    global game_type_label
-    global game_type
-    global menu_continue
     global timer_checkbox
+    global question_amount_optionmenu
+    global game_type_var
+    global game_type_optionmenu
+    global title
+    global category_menu
+    global difficulty_optionmenu
+    global menu_continue
     global timer_var
     global game_type_list
     global categorys
+    global category_var
+    global difficulty_var
+    global question_amount_var
 
     # Main window content
 
-    title = tk.Label(window, text="Trivia Game", font=("Arial", 30))
+    title = tk.Label(window, text="Trivia Game", font=("Arial", 30),bg=bg)
     title.pack(pady=15)
 
     # Category list for the API request 
@@ -309,31 +350,40 @@ def main():
     }
 
     # Category
-    category_label = tk.Label(window, text="Category:")
+    category_label = tk.Label(window, text="Category:",bg=bg)
     category_label.pack()
 
-    menu = ttk.Combobox(window, values=list(categorys.keys()))
-    menu.set("Select a Category")  # Placeholder text
-    menu.pack(pady=4)
+    category_var = tk.StringVar()
+    category_var.set("Select a Category")  # Placeholder text
+
+    category_menu = tk.OptionMenu(window, category_var, *list(categorys.keys()))
+    category_menu.config(bg=bg)
+    category_menu.pack(pady=4)
 
     # Difficulty
-    difficulty_label = tk.Label(window, text="Difficulty:")
+    difficulty_label = tk.Label(window, text="Difficulty:",bg=bg)
     difficulty_label.pack()
 
-    difficultys = ["Any","Easy", "Medium", "Hard"]
-    difficulty = ttk.Combobox(window, values=difficultys)
-    difficulty.set(difficultys[0]) # Set the first item as the placeholder text
-    difficulty.pack(pady=4)
+    difficulties = ["Any","Easy", "Medium", "Hard"]
+    difficulty_var = tk.StringVar()
+    difficulty_var.set(difficulties[0]) # Set the first item as the placeholder text
+
+    difficulty_optionmenu = tk.OptionMenu(window, difficulty_var, *difficulties)
+    difficulty_optionmenu.config(bg=bg)
+    difficulty_optionmenu.pack(pady=4)
 
 
     # Question amount
-    question_amount_label = tk.Label(window, text="Amount of Questions:")
+    question_amount_label = tk.Label(window, text="Amount of Questions:",bg=bg)
     question_amount_label.pack()
 
     question_amount_list = ["5", "10", "20", "25"]
-    question_amounts = ttk.Combobox(window, values=question_amount_list)
-    question_amounts.set(question_amount_list[1]) 
-    question_amounts.pack(pady=4)
+    question_amount_var = tk.StringVar()
+    question_amount_var.set(question_amount_list[1]) 
+
+    question_amount_optionmenu = tk.OptionMenu(window, question_amount_var, *question_amount_list)
+    question_amount_optionmenu.config(bg=bg)
+    question_amount_optionmenu.pack(pady=4)
 
 
     # Game type
@@ -342,31 +392,37 @@ def main():
         "True/False": "boolean"
     }
 
-    game_type_label = tk.Label(window, text="Game type:")
+    game_type_label = tk.Label(window, text="Game type:",bg=bg)
     game_type_label.pack()
 
-    game_type = ttk.Combobox(window, values=list(game_type_list.keys()))
-    game_type.set(list(game_type_list.keys())[0])  # Placeholder text
-    game_type.pack(pady=4)
+    game_type_var = tk.StringVar()
+    game_type_optionmenu = tk.OptionMenu(window, game_type_var, *list(game_type_list.keys()))
+    game_type_var.set(list(game_type_list.keys())[0])  # Placeholder text
+    game_type_optionmenu.config(bg=bg)
+    game_type_optionmenu.pack(pady=4)
 
     # Timer checkbox
     timer_var = tk.IntVar()
 
-    timer_checkbox = tk.Checkbutton(window, text="Timer", variable=timer_var)
+    timer_checkbox = tk.Checkbutton(window, text="Timer", variable=timer_var,borderwidth=0,background=bg)
     timer_checkbox.pack(pady=4)
 
 
-    menu_continue = tk.Button(window, text="Continue", command=fetch_data)
-    menu_continue.pack()
+    # Continue button in the main window
+    menu_continue = tk.Button(window, text="Continue", command=fetch_data,highlightbackground=bg)
+    menu_continue.pack(pady=5)
 
     # Credits
-    def open_link(event):
+    def open_link(event): 
         webbrowser.open("https://opentdb.com/")
 
-    credits = tk.Label(window, text="Q&A from the Open Trivia API", font=("Arial", 12), fg="white", cursor="hand2")
+    credits = tk.Label(window, text="Q&A from the Open Trivia API", font=("Arial", 12), fg="white", cursor="hand2",bg=bg)
     credits.pack(pady=50)
     credits.bind("<Button-1>", open_link)
     credits.pack(pady=50)
 
+# Display the main window
 main()
+
+# Run the GUI
 window.mainloop()
